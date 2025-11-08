231834dca082aef2a2549d9e0bc6f2202b86389a
LewFdtnkv <a_fdtnkv_a@internet.ru>
2025-11-08 17:20:04 +0300

    Fix build and SPA routing for Vercel1

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/people-landing/',
+  base: './',
   server: {
     proxy: {
       '/api': {
